40544457431e5ab32f013b6346d884b38aac17fb
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-07-22 19:43:36 +0300

    correct typo

--- src/js/views/recepView.js
+++ src/js/views/recepView.js
@@ -1,5 +1,5 @@
 import icons from '../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from 'fractional.js';
 import View from './view.js';
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
